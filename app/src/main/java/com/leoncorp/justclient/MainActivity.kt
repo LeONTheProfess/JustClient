65e2da4fe1959b7191d798f6b26ba133dd49c81e
LeON <olm18@yandex.ru>
2021-04-03 20:30:08 +0300

    Почистил код. Добавил вывод ошибок.

--- app/src/main/java/com/leoncorp/justclient/MainActivity.kt
+++ app/src/main/java/com/leoncorp/justclient/MainActivity.kt
@@ -2,24 +2,20 @@ package com.leoncorp.justclient
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.content.res.Resources
-import android.os.AsyncTask
 import android.os.Bundle
-import android.os.Message
-import android.text.Editable
 import android.util.Log
 import android.view.View
 import android.widget.EditText
-import android.widget.TextClock
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.io.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.PrintWriter
 import java.net.Socket
 import java.net.SocketException
 import java.net.UnknownHostException
-import java.nio.charset.Charset
-import java.util.*
-import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private lateinit var prefs: SharedPreferences
@@ -27,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // получаем сохранённые настройки
         prefs = getSharedPreferences("settings", Context.MODE_PRIVATE)
         if (prefs.contains("HOST")){
             findViewById<EditText>(R.id.ipText).setText(prefs.getString("HOST",""))
@@ -37,15 +34,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun buttonClick(view: View){
-
-        val host: String = findViewById<EditText>(R.id.ipText).text.toString()
-        val port: Int = findViewById<EditText>(R.id.portText).text.toString().toInt()
-        val message: String = findViewById<EditText>(R.id.messageText).text.toString()
-        val outText = findViewById<TextView>(R.id.outText)
-        val editor = prefs.edit()
-        editor.putString("HOST", host).apply()
-        editor.putInt("PORT", port).apply()
-        Thread(ClientThread(host,port,message,outText,this)).start()
+        // получаем настройки из полей ввода
+        try{
+            val host: String = findViewById<EditText>(R.id.ipText).text.toString()
+            val port: Int = findViewById<EditText>(R.id.portText).text.toString().toInt()
+            val message: String = findViewById<EditText>(R.id.messageText).text.toString()
+            val outText = findViewById<TextView>(R.id.outText)
+            // сохраняем настройки
+            val editor = prefs.edit()
+            editor.putString("HOST", host).apply()
+            editor.putInt("PORT", port).apply()
+            // запускаем поток отправки/получения сообщения
+            Thread(ClientThread(host,port,message,outText,this)).start()
+        }
+        catch (e: NumberFormatException){
+            val toast = Toast.makeText(applicationContext, "Ошибка. ${e.message}", Toast.LENGTH_SHORT)
+            toast.show()
+            Log.d("Ошибка",e.toString())
+        }
 
     }
 
@@ -63,29 +69,39 @@ class MainActivity : AppCompatActivity() {
                 // Отправка данных на сервер
                 if (message == "") toServer.println("Hello from TEST\n") else toServer.println(message)
                 Log.d("Отправлено", message)
-                //text1.append("Отправлено на сервер\n$iText\n")
+
                 // Ответ сервера
-                val fromServer: BufferedReader = BufferedReader(InputStreamReader(socket.getInputStream()))
+                val fromServer = BufferedReader(InputStreamReader(socket.getInputStream()))
 
                 val line: String = fromServer.readLine()
                 MainActivity.runOnUiThread {
                     outText.text = line
                 }
                 Log.d("Получено",line)
-                //outText.text = line
-                //text1.append("Получено с сервера\n")
-                //text1.append("""$line""".trimIndent())
                 socket.close()
             } catch (e: SocketException) {
+                outToast(e.message)
                 Log.d("Ошибка",e.toString())
                 e.printStackTrace()
             } catch (e: UnknownHostException) {
+                outToast(e.message)
                 Log.d("Ошибка",e.toString())
                 e.printStackTrace()
             } catch (e: IOException) {
+                outToast(e.message)
                 Log.d("Ошибка",e.toString())
                 e.printStackTrace()
             }
+        }
+        
+        //вывод сообщения
+        private fun outToast(message: String?){
+            if (message != null)
+                MainActivity.runOnUiThread {
+                    val toast = Toast.makeText(MainActivity.applicationContext,
+                        "Ошибка. $message", Toast.LENGTH_LONG)
+                    toast.show()
+                }
         }
 
     }
